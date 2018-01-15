dfc36154caa5afc0d1990d97b175a612d8063ba2
chen feng <chenfengcty@gmail.com>
2018-01-15 19:54:38 +1100

    my first code

---
+++ mysite/qablog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    writer = models.CharField(max_length=50)
+    created_date = models.DateField(auto_now_add=True)
+    modify_date = models.DateField(auto_now=True)
+    content = models.TextField()
+    is_show = models.BooleanField()
+
+    class Meta:
+        db_table = "article"
+
+    def __str__(self):
+        return self.title
