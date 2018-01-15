ac866a1ce32f2d0dc75480a5dec9273fbe510c68
chen feng <chenfengcty@gmail.com>
2018-01-15 22:31:38 +1100

    second

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from qablog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home), # mainpage
+    path('blog/', views.home), #blog page
 ]
--- mysite/qablog/views.py
+++ mysite/qablog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def home(request):
+    # now = datetime.datetime.now()
+
+    return render(request, 'home.html', {})
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
